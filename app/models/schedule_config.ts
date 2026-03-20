4dbbb56816ed2105404ae884f9a500e41e04b416
Gustavo Souza <gustavo.araujo@aluno.uepa.br>
2026-03-20 13:49:50 -0300

    fixed schedule config table

--- app/models/schedule_config.ts
+++ app/models/schedule_config.ts
@@ -2,6 +2,7 @@ import { DateTime } from 'luxon'
 import { BaseModel, column } from '@adonisjs/lucid/orm'
 
 export default class ScheduleConfig extends BaseModel {
+  static table = 'schedule_config'
   @column({ isPrimary: true })
   declare id: number
 
